917c7d88e2af1880eefff06816f11aa02caf790b
Julia0601 <126571491+Julia0601@users.noreply.github.com>
2023-03-13 18:54:57 +0200

    Person
    Java Basic/ Homework 0

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+
+    public static void main(String[] args) {
+        String name = "Julia";
+        String LastName = "Drabataya";
+        int age = 18;
+
+        System.out.println(name);
+        System.out.println(LastName);
+        System.out.println(age);
+
+
+    }
+
+
+}
