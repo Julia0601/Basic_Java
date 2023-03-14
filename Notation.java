4bd751c244964d046427e709c427964493f7e97c
Julia0601 <126571491+Julia0601@users.noreply.github.com>
2023-03-14 18:25:27 +0200

    Notation
    Java Basic / Homework 1

---
+++ Notation.java
@@ -0,0 +1,53 @@
+public class Notation {
+
+    public static void main(String[] args) {
+
+        int a = 1144;
+        System.out.println(1144 / 8); //143
+        System.out.println(1144 % 8); //0
+
+        System.out.println(143 / 8); //17
+        System.out.println(143 % 8); //7
+
+        System.out.println(17 / 8); //2
+        System.out.println(17 % 8); //1
+
+        System.out.println(2 / 8); //0
+        System.out.println(2 % 8); //2
+
+        int a8 = 2170;
+        System.out.println(a8);
+        System.out.println(2*Math.pow(8, 3) + 1*Math.pow(8, 2) + 7*Math.pow(8, 1) + 0*Math.pow(8, 0));
+
+        int t = 978;
+        System.out.println(978 / 8); //122
+        System.out.println(978 % 8); //2
+
+        System.out.println(122 / 8); //15
+        System.out.println(122 % 8); //2
+
+        System.out.println(15 / 8); //1
+        System.out.println(15 % 8); //7
+
+        System.out.println(1 / 8); //0
+        System.out.println(1 % 8); //1
+
+        int t8 = 1722;
+        System.out.println(t8);
+        System.out.println(1*Math.pow(8, 3) + 7*Math.pow(8, 2) + 2*Math.pow(8, 1) + 2*Math.pow(8, 0));
+
+        int r = 12;
+        System.out.println(12 / 8); //1
+        System.out.println(12 % 8); //4
+
+        System.out.println(1 / 8); //0
+        System.out.println(1 % 8); //1
+
+        int r8 = 014;
+        System.out.println(r8);
+
+
+
+
+    }
+}
