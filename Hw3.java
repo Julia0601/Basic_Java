916929f664da653d7ff5ed796250eb2d28e56e16
Julia0601 <126571491+Julia0601@users.noreply.github.com>
2023-03-20 21:23:32 +0200

    Hw3
    Java Basic / Homework 3

---
+++ Hw3.java
@@ -0,0 +1,9 @@
+public class Hw3 {
+    public static void main(String[] args) {
+        String u = "Hello, Java! It's a Java Basic course";
+        System.out.println(u.length());
+        System.out.println(u.charAt(11));
+        System.out.println(u.substring(13,24));
+        System.out.println(u.substring(20,37));
+    }
+}
